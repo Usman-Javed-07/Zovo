5e66a67cf52eb9a16277ac159324c1d258ed1e53
Usman Javed <usmanjaved0816@gmail.com>
2026-02-25 20:13:53 +0500

    add silder and trending products

--- client/script/script.js
+++ client/script/script.js
@@ -3,3 +3,21 @@ const links = document.querySelectorAll(".navbar a");
 links.forEach((link) => {
   if (link.href === window.location.href) link.classList.add("active");
 });
+
+ const swiper = new Swiper(".heroSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 800, // smooth slide speed
+    direction: "horizontal", // slide left-right
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
